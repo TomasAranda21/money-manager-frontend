e2bbd7c4eaea9561c1f1c1d6be9f4e9f4642d68f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-29 19:28:56 -0300

    add button to switch between type of operation

---
+++ src/components/Buttons/ButtonsTypeTransaction.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ButtonsTypeTransaction = ({type, onClick}) => {
+
+  return ( 
+    <>
+    
+    <button onClick={onClick}
+    className={`text-center font-bold text-2xl uppercase border-2 p-1 px-3
+    focus:border-b-4 active:border-b-4 ${type === true ? 'focus:border-red-500' : 'focus:border-cyan-500'} `}>
+      
+    {` ${type === true ? 'Expense 💸' : 'Income 💵'}`}</button>
+    
+    </>
+
+  )
+
+}
+
+export default ButtonsTypeTransaction
