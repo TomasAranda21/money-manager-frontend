ed6867192120214c46b25314e38a60cabad7d31f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-29 17:14:48 -0300

    add context and hooks to operations

---
+++ src/hooks/useOperation.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import OperationContext from '../context/OperationContext'
+
+const useOperation = () => {
+
+    return useContext(OperationContext)
+
+}
+
+export default useOperation
