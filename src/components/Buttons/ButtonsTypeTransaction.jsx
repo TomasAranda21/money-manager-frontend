98a10920eb88d9562b3fdbd953dba6935d94af81
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-30 20:11:41 -0300

    change border style

--- src/components/Buttons/ButtonsTypeTransaction.jsx
+++ src/components/Buttons/ButtonsTypeTransaction.jsx
@@ -6,8 +6,9 @@ const ButtonsTypeTransaction = ({type, onClick}) => {
     <>
     
     <button onClick={onClick}
-    className={`text-center font-bold text-lg md:text-2xl uppercase border-2 p-1 px-3
-    focus:border-b-4 active:border-b-4 ${type === true ? 'focus:border-red-500' : 'focus:border-cyan-500'} `}>
+    className={`text-center font-bold text-lg md:text-2xl uppercase border-2 focus:border focus:border-gray-600 duration-100 p-1 px-3
+
+    border-b-4 active:border-b-4 ${type === true ? 'border-red-500' : 'border-cyan-500'} `}>
       
     {` ${type === true ? 'Expense 💸' : 'Income 💵'}`}</button>
     
