0a579de7fe0e090311ba096639c96607c26da5c5
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-30 00:21:59 -0300

    feat add modals and buttons

--- src/pages/HomeUser.jsx
+++ src/pages/HomeUser.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ButtonsTypeTransaction from '../components/Buttons/ButtonsTypeTransaction'
+import ButtonTransaction from '../components/Buttons/ButtonTransaction'
+import ModalAddOperation from '../components/Modals/ModalAddOperation'
+import useAuth from '../hooks/useAuth'
+import useOperation from '../hooks/useOperation'
 
 const HomeUser = () => {
+
+
+    const { auth } = useAuth()
+    const {addOperation, operations} = useOperation()
+
+    const [viewModal, setViewModal] = useState(false)
+
   return (
-    <div>HomeUser</div>
+
+    <div className="pb-10">
+            <h2 className="text-center text-2xl md:text-3xl capitalize font-bold mb-6">{`👋 Hi ${auth.name}!`}</h2>
+
+            <div className="flex justify-center mb-8">
+
+                <ButtonTransaction text="Add Transaction" onClick={() => setViewModal(true)}/>
+         
+            </div>
+
+        <div className='flex gap-5 md:gap-10 justify-center mb-2'>
+
+            <ButtonsTypeTransaction type={false}/>
+            <ButtonsTypeTransaction type={true}/>
+          
+        </div>
+
+
+        {viewModal && <ModalAddOperation setViewModal={setViewModal} addOperation={addOperation}/>}
+
+
+    </div>
+
+
   )
 }
 
