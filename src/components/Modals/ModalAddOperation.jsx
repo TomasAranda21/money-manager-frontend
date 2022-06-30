a7f2d87f0460b20bc51b2db72ce99d8b0a5a226c
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-30 00:16:07 -0300

    change in size for different divices

--- src/components/Modals/ModalAddOperation.jsx
+++ src/components/Modals/ModalAddOperation.jsx
@@ -27,13 +27,13 @@ const ModalAddOperation = ({setViewModal, addOperation}) => {
 
   return (
 
-    <div className="absolute top-0 w-full h-screen left-0 backdrop-brightness-50">
+    <div className="absolute top-0 w-full h-screen left-0 backdrop-brightness-50 px-2">
 
-     <div className="shadow-2xl bg-gray-100 flex justify-center my-32 w-1/3 mx-auto py-20 relative rounded-md border-2 border-teal-200">
+     <div className="shadow-2xl bg-gray-100 flex justify-center my-20 md:my-32 md:w-1/3 mx-auto py-20 relative rounded-md border-2 border-teal-200">
 
         {isExpense === true || isIncome === true ?
 
-            <div className="absolute top-0 left-0 p-2 px-5 font-bold">
+            <div className="absolute top-0 left-0 p-5 px-5 font-bold">
                 <button className="flex items-center gap-2 text-gray-500 hover:text-gray-900 duration-200"
                 onClick={() => handleComeBack(false)}> <MdArrowBack/> Return </button>
             </div>
@@ -63,7 +63,7 @@ const ModalAddOperation = ({setViewModal, addOperation}) => {
 
             {isExpense &&
 
-                <div className="w-full px-20">
+                <div className="w-full px-4 lg:px-20">
                     <FormAddOperation
                     setViewModal = {setViewModal} 
                     functionUser={addOperation}
@@ -85,7 +85,7 @@ const ModalAddOperation = ({setViewModal, addOperation}) => {
 
             {isIncome &&
 
-                <div className="w-full px-20">
+                <div className="w-full px-4 lg:px-20">
 
                     <FormAddOperation 
                     setViewModal = {setViewModal} 
