767a4089392e3c0031a723dbdf2d6fc783bf4668
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-30 00:20:34 -0300

    feat add regular expression for budget

--- src/helpers/Exp_reg.jsx
+++ src/helpers/Exp_reg.jsx
@@ -4,6 +4,7 @@ const exp_reg = {
     name_exp: /^[a-zA-ZÁ-ÿ-0-9\s]{0,50}$/,
     pass_exp: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,15}$/,
     email_exp: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    budget_exp: /^[,-.-0-9\s]{0,12}$/,
 
 
 }
