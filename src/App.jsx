f64b66d851ded4054479a0e1a995bbf6403b69d1
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-03 13:56:38 -0300

    add routes to send email and recover password

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,11 @@ import { OperationProvider } from './context/OperationContext'
 import AuthLayout from './layout/AuthLayout'
 import Login from './pages/LoginUser'
 import Register from './pages/RegisterUser'
+import ForgotPass from './pages/ForgotPass'
+import NewPass from './pages/NewPass'
 
 
 // Private path
-
 import PrivateLayout from './layout/PrivateLayout'
 import Home from './pages/HomeUser'
 import History from './pages/HistoryUser'
@@ -40,6 +41,10 @@ function App() {
              
             
                   <Route path="sign-in" element={ <Register />} />
+
+                  <Route path="forgot-password" element={ <ForgotPass />} />
+
+                  <Route path="new-password/:token" element={ <NewPass />} />
 
                   <Route path="confirm-account/:token" element={ <ConfirmAccount />} />
           
